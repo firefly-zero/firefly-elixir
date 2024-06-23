cb58c316e9c361ed65810e09ee6933d351730395
gram <git@orsinium.dev>
2024-06-23 13:20:47 +0200

    support atoms for colors

--- lib/firefly.ex
+++ lib/firefly.ex
@@ -27,9 +27,29 @@ defmodule Firefly do
       b.y,
       c.x,
       c.y,
-      s.fill_color,
-      s.stroke_color,
+      parse_color(s.fill_color),
+      parse_color(s.stroke_color),
       s.stroke_width
     )
   end
+
+  defp parse_color(nil), do: 0
+  defp parse_color(:none), do: 0
+  defp parse_color(:black), do: 1
+  defp parse_color(:purple), do: 2
+  defp parse_color(:red), do: 3
+  defp parse_color(:orange), do: 4
+  defp parse_color(:yellow), do: 5
+  defp parse_color(:light_green), do: 6
+  defp parse_color(:green), do: 7
+  defp parse_color(:dark_green), do: 8
+  defp parse_color(:dark_blue), do: 9
+  defp parse_color(:blue), do: 10
+  defp parse_color(:light_blue), do: 11
+  defp parse_color(:cyan), do: 12
+  defp parse_color(:white), do: 13
+  defp parse_color(:light_gray), do: 14
+  defp parse_color(:gray), do: 15
+  defp parse_color(:dark_gray), do: 16
+  defp parse_color(x) when is_integer(x) and x <= 16 and x >= 0, do: x
 end
--- lib/triangle.ex
+++ lib/triangle.ex
@@ -6,7 +6,7 @@ defmodule Firefly.Demo.Triangle do
       %Point{x: 60, y: 10},
       %Point{x: 40, y: 40},
       %Point{x: 80, y: 40},
-      %Style{fill_color: 14, stroke_color: 9, stroke_width: 1}
+      %Style{fill_color: :light_gray, stroke_color: :dark_blue, stroke_width: 1}
     )
   end
 end
