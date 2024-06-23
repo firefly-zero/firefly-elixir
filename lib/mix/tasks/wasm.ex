451f4c2907f181d692d23aad85c6b788b3cc77cc
gram <git@orsinium.dev>
2024-06-23 12:18:42 +0200

    accept module name as CLI arg

--- lib/mix/tasks/wasm.ex
+++ lib/mix/tasks/wasm.ex
@@ -1,10 +1,14 @@
 defmodule Mix.Tasks.Wasm do
-  @moduledoc "The wasm mix task: `mix help wasm`"
+  @moduledoc "Compile the given module to WebAssembly."
   use Mix.Task
 
-  @shortdoc "Compile the project to WebAssembly."
-  def run(_) do
-    Orb.to_wat(Firefly.Demo.Triangle) |> IO.puts()
+  def run(args) do
+    if Enum.empty?(args) do
+      raise "module name must be provided"
+    end
+
+    mod = "Elixir." <> Enum.at(args, 0)
+    mod |> String.to_atom() |> Orb.to_wat() |> IO.puts()
     # File.write("main.wasm", Orb.to_wasm(Firefly))
     # IO.puts("✅ saved to main.wasm")
   end
